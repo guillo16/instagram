6c209c990f96dd9198c7a18fd7942566ad1f6f0b
guillermo varela <guillovarela16@gmail.com>
2019-08-22 16:46:00 -0300

    pundit finish

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
+    @posts = policy_scope(Post).order(created_at: :desc)
   end
 
   def show
@@ -9,11 +9,13 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    authorize(@post)
   end
 
   def create
     @post = Post.new(post_params)
     @post.user = current_user
+    authorize @post
     if @post.save
       redirect_to posts_path
     else
@@ -25,6 +27,7 @@ class PostsController < ApplicationController
   end
 
   def update
+    authorize @post
     if @post.update(post_params)
       redirect_to posts_path
     else
@@ -33,6 +36,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    authorize @post
     @post.destroy
     redirect_to posts_path
   end
@@ -41,6 +45,7 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+    authorize @post
   end
 
   def post_params
