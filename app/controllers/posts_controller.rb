a8138f41344b42ca719210ec952ce17d2b9f57f4
guillermo varela <guillovarela16@gmail.com>
2019-08-22 15:42:31 -0300

    finish post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,42 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :create, :edit, :destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts.path
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post.detroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:posts).permit(:title, :photo)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :photo, presence: true
 end
