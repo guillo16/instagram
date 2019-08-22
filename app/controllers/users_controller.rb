aad83f4460fb95adc6be231811c6039127f68e0f
guillermo varela <guillovarela16@gmail.com>
2019-08-22 19:59:14 -0300

    generate policy user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'pages/about'
   root to: 'posts#index'
   resources :posts
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
