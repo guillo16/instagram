9a07c509d407524f809b2364fee8cb57bde9de8c
guillermo varela <guillovarela16@gmail.com>
2019-08-24 11:45:34 -0300

    add reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/about'
   root to: 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :reviews, only: :create
+  end
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
