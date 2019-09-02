ec5d31c68999062791f1a2b5d0c9917fc7ac995f
guillermo varela <guillovarela16@gmail.com>
2019-09-02 19:44:30 -0300

    followers methos applied

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = policy_scope(Post).order(created_at: :desc)
+
   end
 
   def show
