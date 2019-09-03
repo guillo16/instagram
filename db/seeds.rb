0c00115ed6a06dec83371432d542d3354b0789ae
guillermo varela <guillovarela16@gmail.com>
2019-09-03 20:00:16 -0300

    last seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,23 +30,23 @@ User.create(email: "carlos1@hotmail.com",
 puts 'finish user'
 
 
-Post.create(user_id: 19, remote_photo_url: "https://images.unsplash.com/photo-1529680459049-bf0340fa0755?ixlib=rb-1.2.1&auto=format&fit=crop&w=1268&q=80", title: "the best")
-Post.create(user_id: 20,
+Post.create(user_id: 15, remote_photo_url: "https://images.unsplash.com/photo-1529680459049-bf0340fa0755?ixlib=rb-1.2.1&auto=format&fit=crop&w=1268&q=80", title: "the best")
+Post.create(user_id: 16,
  remote_photo_url: "https://images.unsplash.com/photo-1529680459049-bf0340fa0755?ixlib=rb-1.2.1&auto=format&fit=crop&w=1268&q=80",
  title: "the best")
-Post.create(user_id: 22,
+Post.create(user_id: 17,
  remote_photo_url: "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
  title: "New york")
-Post.create(user_id: 23,
+Post.create(user_id: 18,
  remote_photo_url: "https://images.unsplash.com/photo-1470219556762-1771e7f9427d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80" ,
  title: "Manhattan")
-Post.create(user_id: 22,
+Post.create(user_id: 15,
  remote_photo_url: "https://images.unsplash.com/photo-1527956041665-d7a1b380c460?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1268&q=80" ,
  title: "City of dreams")
-Post.create(user_id: 22,
+Post.create(user_id: 16,
  remote_photo_url: "https://images.unsplash.com/photo-1527362385507-e99d6bf2a2a1?ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
  title: "guillo")
-Post.create(user_id: 22,
+Post.create(user_id: 18,
  remote_photo_url: "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
  title: "Dogs")
 # Post.create(user_id: ,
