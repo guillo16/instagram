380da9d3de042dedd07fb42b4b5164c3fb37032d
guillermo varela <guillovarela16@gmail.com>
2019-09-06 16:04:44 -0300

    liking

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,34 @@
 class LikesController < ApplicationController
   before_action :find_post
+  before_action :find_like, only: [:destroy]
+
 
   def create
-    @post.likes.create(user_id: current_user.id)
+    @post = Post.find(params[:post_id])
+    @like = Like.new
+    @like.post = @post
+    @like.user = current_user
+    authorize @like
+    if @like.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'posts/show' }
+        format.js # <-- idem
+      end
+    end
+  end
+
+  def destroy
+    authorize @like
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @like.destroy
+    end
     redirect_to post_path(@post)
   end
 
@@ -10,5 +36,12 @@ class LikesController < ApplicationController
 
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+      params[:post_id]).exists?
   end
 end
--- app/policies/like_policy.rb
+++ app/policies/like_policy.rb
@@ -4,4 +4,12 @@ class LikePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    return true
+  end
+
+  def destroy?
+    record.user == user
+  end
 end
